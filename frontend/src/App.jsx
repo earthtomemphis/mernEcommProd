692248c14d82913b3db1344d0b0e1e091814e8e2
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-08-07 21:04:33 -0600

    header and footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to Proshop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
+};
+
+export default App;
